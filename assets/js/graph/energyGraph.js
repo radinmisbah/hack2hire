562a0f6dd510f2b168c560fba3cbc8def57b94c3
radinmisbah <radin01misbah@gmail.com>
2023-10-16 13:38:02 +0800

    add graph

--- assets/js/graph/energyGraph.js
+++ assets/js/graph/energyGraph.js
@@ -1,67 +1,20 @@
 $(document).ready(function(){
-    $.ajax({
-      url : "http://localhost/chartjs/followersdata.php",
-      type : "GET",
-      success : function(data){
-        console.log(data);
-  
-        var userid = [];
-        var facebook_follower = [];
-        var twitter_follower = [];
-        var googleplus_follower = [];
-  
-        for(var i in data) {
-          userid.push("UserID " + data[i].userid);
-          facebook_follower.push(data[i].facebook);
-          twitter_follower.push(data[i].twitter);
-          googleplus_follower.push(data[i].googleplus);
-        }
-  
-        var chartdata = {
-          labels: userid,
-          datasets: [
-            {
-              label: "facebook",
-              fill: false,
-              lineTension: 0.1,
-              backgroundColor: "rgba(59, 89, 152, 0.75)",
-              borderColor: "rgba(59, 89, 152, 1)",
-              pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
-              pointHoverBorderColor: "rgba(59, 89, 152, 1)",
-              data: facebook_follower
-            },
-            {
-              label: "twitter",
-              fill: false,
-              lineTension: 0.1,
-              backgroundColor: "rgba(29, 202, 255, 0.75)",
-              borderColor: "rgba(29, 202, 255, 1)",
-              pointHoverBackgroundColor: "rgba(29, 202, 255, 1)",
-              pointHoverBorderColor: "rgba(29, 202, 255, 1)",
-              data: twitter_follower
-            },
-            {
-              label: "googleplus",
-              fill: false,
-              lineTension: 0.1,
-              backgroundColor: "rgba(211, 72, 54, 0.75)",
-              borderColor: "rgba(211, 72, 54, 1)",
-              pointHoverBackgroundColor: "rgba(211, 72, 54, 1)",
-              pointHoverBorderColor: "rgba(211, 72, 54, 1)",
-              data: googleplus_follower
-            }
-          ]
-        };
-  
-        var ctx = $("#mycanvas");
-  
-        var LineGraph = new Chart(ctx, {
-          type: 'line',
-          data: chartdata
-        });
-      },
-      error : function(data) {
-  
-      }
-    });
+   // chart.js
+const labels = ['January', 'February', 'March', 'April', 'May'];
+const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First Dataset',
+        backgroundColor: 'rgb(75, 192, 192)',
+        borderColor: 'rgb(75, 192, 192)',
+        data: [65, 59, 80, 81, 56],
+    }]
+};
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+});
+
   });
